8deca031375db37a4f1a61d2197533e18cfdf2bd
pauloagsantos <pauloagsantos@gmail.com>
2024-10-18 17:36:43 +0100

    camaras

--- Camara1/app/src/main/java/ipt/dam/camarav1/MainActivity.kt
+++ Camara1/app/src/main/java/ipt/dam/camarav1/MainActivity.kt
@@ -1,15 +1,44 @@
 package ipt.dam.camarav1
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+    lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button = findViewById(R.id.button)
+        imageView = findViewById(R.id.imageView)
+
+        button.setOnClickListener {
+            capturePhoto()
+        }
+    }
+
+    fun capturePhoto() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        resultLauncher.launch(cameraIntent)
+    }
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            imageView.setImageBitmap(data?.extras?.get("data") as Bitmap)
+        }
     }
 }
